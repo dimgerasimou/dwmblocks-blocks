15ea4fbf74665ac97fc033847f47cf120f4a13da
Dimitris Gerasimou <gerasimou-gh@posteo.gr>
2026-01-12 00:30:44 +0200

    [Feature] Add catppuccin-mono theme

---
+++ themes/catppuccin-mono.h
@@ -0,0 +1,25 @@
+/* See LICENSE file for copyright and license details. */
+/* theme: catppuccin-mono */
+
+#ifndef COLORSCHEME_H
+#define COLORSCHEME_H
+
+#define CLR_NRM     "^d^"
+#define CLR_BAT_CRT "^c#F38BA8^" /* red */
+#define CLR_BAT_LOW "^c#F9E2AF^" /* yellow */
+#define CLR_BAT_NRM "^c#CDD6F4^" /* white */
+#define CLR_BAT_CHG "^c#F9E2AF^" /* yellow */
+#define CLR_BT      "^c#CDD6F4^" /* white */
+#define CLR_DATE    "^c#CDD6F4^" /* white */
+#define CLR_NET_NRM "^c#CDD6F4^" /* white */
+#define CLR_NET_ERR "^c#F38BA8^" /* red */
+#define CLR_KRN_PKG "^c#89B4FA^" /* blue */
+#define CLR_KRN_NRM "^c#CDD6F4^" /* white */
+#define CLR_KBD     "^c#CDD6F4^" /* white */
+#define CLR_MEM     "^c#CDD6F4^" /* white */
+#define CLR_PWR     "^c#F38BA8^" /* red */
+#define CLR_TIM     "^c#CDD6F4^" /* white */
+#define CLR_VOL_NRM "^c#CDD6F4^" /* white */
+#define CLR_VOL_MUT "^c#F38BA8^" /* red */
+
+#endif /* COLORSCHEME_H */
